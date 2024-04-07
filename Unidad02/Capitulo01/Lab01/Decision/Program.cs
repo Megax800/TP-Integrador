316c0aece4358fc44af50b6b859707e1f455bd51
CoolAir <128939197+Megax800@users.noreply.github.com>
2024-04-07 18:34:52 -0300

    Add files via upload

---
+++ Unidad02/Capitulo01/Lab01/Decision/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+string InputTexto;
+Console.WriteLine("Ingrese un Texto:");
+InputTexto= Console.ReadLine();
+
+while(InputTexto == ""){
+    Console.WriteLine("Ingrese un Texto:");
+    InputTexto = Console.ReadLine();
+}
+
+bool salir = false;
+
+Console.WriteLine("Texto OK");
+while (salir == false)
+{
+    Console.WriteLine("Menu:\n 1. Texto en Mayusculas\n 2. Texto en Minusculas\n 3.Cantidad de Caracteres\n 0. Salir\n");
+    
+    ConsoleKeyInfo opcion = Console.ReadKey();
+
+    if (opcion.Key == ConsoleKey.D1)
+    {
+        Console.WriteLine(InputTexto.ToUpper());
+    }
+    else if (opcion.Key == ConsoleKey.D2)
+    {
+        Console.WriteLine(InputTexto.ToLower());
+    }
+    else if (opcion.Key == ConsoleKey.D3)
+    {
+        Console.WriteLine(InputTexto.Length);
+    }
+    else if (opcion.Key == ConsoleKey.D0)
+    {
+        salir = true;
+    }
+    else
+    {
+        Console.WriteLine("Valor no Valido");
+    }
+}
