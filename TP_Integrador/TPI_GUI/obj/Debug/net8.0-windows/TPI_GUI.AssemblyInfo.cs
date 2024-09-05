0a06823e877e8d62823343dc8d9a5317bb57bdb0
Octavio <rusmanoctavio@gmail.com>
2024-09-05 11:37:43 -0300

    modificaciones menores en el menu general y ABM usuario,

--- TP_Integrador/TPI_GUI/obj/Debug/net8.0-windows/TPI_GUI.AssemblyInfo.cs
+++ TP_Integrador/TPI_GUI/obj/Debug/net8.0-windows/TPI_GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPI_GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2534127c87f70929421016b27291a6add03830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0338d62e216399f1461ceb6c4e06332724743195")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPI_GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPI_GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
